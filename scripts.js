0a679c2c1e74d9c32c957cb459d79b88a7d666da
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-07-11 09:17:32 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const prompts = [
-    "Draw a dragon",
+"Draw a dragon",
     "Sketch your dream house",
     "Create a new superhero",
     "Illustrate a scene from your favorite book",
@@ -8,7 +8,27 @@ const prompts = [
     "Create a new type of plant",
     "Draw a day in the life of a cat",
     "Illustrate your favorite holiday",
-    "Draw a magical forest"
+    "Draw a magical forest",
+    "Draw a castle in the clouds",
+    "Sketch a robot with a unique function",
+    "Create a new mythical creature",
+    "Illustrate a scene from a carnival",
+    "Draw a space station on a distant planet",
+    "Sketch an enchanted treehouse",
+    "Create a superhero's secret lair",
+    "Draw a magical potion with strange ingredients",
+    "Sketch an underwater adventure",
+    "Illustrate a day at the zoo",
+    "Draw a landscape from another planet",
+    "Create a new kind of flower",
+    "Sketch a scene from your favorite movie",
+    "Draw a futuristic city skyline",
+    "Illustrate a fairy tale character in modern times",
+    "Draw an animal doing something unusual",
+    "Create a new type of transportation",
+    "Sketch a scene from a beach",
+    "Draw a magical doorway to another world",
+    "Illustrate your dream vacation spot"
 ];
 
 let usedPrompts = [];
